4feefa140fe932ba313b8085d0e2e9b74a6924a4
Laurel Thorburn <codinglaurel@gmail.com>
2022-12-03 14:28:39 -0700

    Day2 Part 2

--- December-2.js
+++ December-2.js
@@ -16,13 +16,55 @@ const scoreDictionary = {
 // "A" & "Z" || "B" & "X" || "C" & "Y" = lose
 // "A" & "Y" || "B" & "Z" || "C" & "X" = win
 
-const gameResult = (elfchoice, myChoice) => {
-    if ((elfchoice === "A" && myChoice === "X") || (elfchoice === "B" && myChoice === "Y") || (elfchoice === "C" && myChoice === "Z")) {
+const gameResult = (elfChoice, myChoice) => {
+    if ((elfChoice === "A" && myChoice === "X") || (elfChoice === "B" && myChoice === "Y") || (elfChoice === "C" && myChoice === "Z")) {
         return "DRAW"
-    } else if ((elfchoice === "A" && myChoice === "Z") || (elfchoice === "B" && myChoice === "X") || (elfchoice === "C" && myChoice === "Y")) {
+    } else if ((elfChoice === "A" && myChoice === "Z") || (elfChoice === "B" && myChoice === "X") || (elfChoice === "C" && myChoice === "Y")) {
         return "LOSE"
-    } else if ((elfchoice === "A" && myChoice === "Y") || (elfchoice === "B" && myChoice === "Z") || (elfchoice === "C" && myChoice === "X")) {
+    } else if ((elfChoice === "A" && myChoice === "Y") || (elfChoice === "B" && myChoice === "Z") || (elfChoice === "C" && myChoice === "X")) {
         return "WIN"
+    }
+    return "error, laurel messed up" // can default to win but wanted to log this to catch my mistakes
+}
+
+// const myResults = [];
+
+// rounds.map((round) => {
+//     const result = scoreDictionary[gameResult(round[0], round[1])];
+//     const myChoice = scoreDictionary[round[1]];
+
+//     myResults.push(result, myChoice);
+// });
+
+// const initialValue = 0;
+// const totalScore = myResults.reduce((accumulator, currentValue) => accumulator + currentValue, initialValue)
+
+// console.log({totalScore})
+
+// part 2, now the game has changed... X means you need to lose, Y means you need to end the round in a draw, and Z means you need to win. Good luck!"
+
+const updatedDictionary = {
+    "X": 0, // lose
+    "Y": 3, // draw
+    "Z": 6, // win
+    "A": 1, // rock
+    "B": 2, // paper
+    "C": 3 // scissors
+}
+
+// "A": rock, "B": paper, "C": scissors
+// "A" & "X" || "C" & "Y" || "B" & "Z" = "C"
+// "B" & "Y" || "A" & "Z" || "C" & "X" = "B"
+// "C" & "Z" || "B" & "X" || "A" & "Y" = "A"
+
+const myPlay = (elfChoice, gameResult) => {
+    if ((elfChoice === "A" && gameResult === "X") || (elfChoice === "C" && gameResult === "Y") || (elfChoice === "B" && gameResult === "Z")){
+        return "C"
+    } else if ((elfChoice === "B" && gameResult === "Y" )|| (elfChoice === "A" && gameResult === "Z") || (elfChoice === "C" && gameResult === "X")){
+        return "B"
+    }
+    else if ((elfChoice === "C" && gameResult === "Z") || (elfChoice === "B" && gameResult === "X") || (elfChoice === "A" && gameResult === "Y") ){
+        return "A"
     }
     return "error, laurel messed up" // can default to win but wanted to log this to catch my mistakes
 }
@@ -30,8 +72,8 @@ const gameResult = (elfchoice, myChoice) => {
 const myResults = [];
 
 rounds.map((round) => {
-    const result = scoreDictionary[gameResult(round[0], round[1])];
-    const myChoice = scoreDictionary[round[1]];
+    const result = updatedDictionary[myPlay(round[0], round[1])];
+    const myChoice = updatedDictionary[round[1]];
 
     myResults.push(result, myChoice);
 });
